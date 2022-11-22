3aba0909922fb5cf92cbf940f98fe69648b4e082
Renat Skosarev <skosarrrev@gmail.com>
2022-11-23 02:32:25 +0500

    Git и случайные окружности

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import sys
 
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5 import uic
+
+from UI import Ui_MainWindow
 
 
-class Window(QMainWindow):
+class Window(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(Window, self).__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.flag = False
         self.some_button.clicked.connect(self.paint_yellow_circles)
 
@@ -21,12 +22,17 @@ class Window(QMainWindow):
         if self.flag:
             painter = QPainter()
             painter.begin(self)
-            painter.setPen(QColor('yellow'))
             for i in range(5):
-                painter.setBrush(QColor(255, 255, 0, random.randint(30, 150)))
                 x = random.randint(0, self.width() - 100)
                 y = random.randint(0, self.height() - 100)
                 diameter = random.randint(50, 200)
+                r = random.randint(0, 255)
+                g = random.randint(0, 255)
+                b = random.randint(0, 255)
+                transparency = random.randint(30, 200)
+                color = QColor(r, g, b, transparency)
+                painter.setPen(color)
+                painter.setBrush(color)
                 painter.drawEllipse(x, y, diameter, diameter)
             painter.end()
             self.flag = False
